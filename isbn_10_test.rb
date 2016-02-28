256cb60495169e2baec7c40edb60ad5834268de5
Edward Helman <eddhelman@gmail.com>
2016-02-28 10:54:16 -0500

    test 2

--- isbn_10_test.rb
+++ isbn_10_test.rb
@@ -4,7 +4,7 @@ require_relative "isbn_10.rb"
 class TestIsbn < Minitest:: Test 
 
 	def test_string_is_valid_amount_of_digits
-		#assert_equal(true, isbn_length("9780470059029"))
+		assert_equal(true, isbn_length("9780470059029"))
 		assert_equal(false, isbn_length("478047005902"))
 	end
 		
